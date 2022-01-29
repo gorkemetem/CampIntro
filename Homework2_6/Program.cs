3a19ef7f940b4e8829ce3318c0aac8fac4c20e45
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2022-01-30 00:15:27 +0300

    Add project files.

---
+++ Homework2_6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Homework2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.ad = "ÜRÜN1";
+            urun1.fiyat = 15;
+            urun1.indirim = "indirim1";
+
+            Product urun2 = new Product();
+            urun2.ad = "ÜRÜN2";
+            urun2.fiyat = 17;
+            urun2.indirim = "indirim2";
+
+            Product urun3 = new Product();
+            urun3.ad = "ÜRÜN3";
+            urun3.fiyat = 16;
+            urun3.indirim = "indirim3";
+
+            Product[] urunler = new Product[] {urun1, urun2, urun3 };
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+                Console.WriteLine(urunler[i].ad + " - " + urunler[i].fiyat);
+            }
+
+            Console.WriteLine("******************************");
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.ad + " - " + urun.fiyat);
+            }
+
+            Console.WriteLine("******************************");
+
+            int j = 0;
+            while (j < urunler.Length)
+            {
+                Console.WriteLine(urunler[j].ad + " - " + urunler[j].fiyat);
+                j++;
+            }
+        }
+    }
+
+    class Product
+    {
+        public string ad { get; set; }
+        public int fiyat { get; set; }
+        public string indirim { get; set; }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.32;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+          
+
+        }
+    }
+}
